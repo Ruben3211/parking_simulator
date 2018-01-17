60d03c586d3c4bbe0052edd0622cdbd733dbb76a
Ruben Bonga <r.a.bonga@st.hanze.nl>
2018-01-17 11:53:27 +0100

    Hello world Printline

--- src/parkingsimulator/HelloWorld.java
+++ src/parkingsimulator/HelloWorld.java
@@ -1,5 +1,11 @@
 package parkingsimulator;
 // Rick was here.
 public class HelloWorld {
-
-}
+private String Hello;
+	public HelloWorld() {
+	Hello = "Hello World";
+	}
+	public void printHello() {
+		System.out.println(Hello);
+	}
+	}
