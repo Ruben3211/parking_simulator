a73e691bc9a4e00c10690f325712bc812eb6959e
Detmer Struiksma <p.detmer56@gmail.com>
2018-01-17 12:38:17 +0100

    hallo!

--- src/parkingsimulator/HelloWorld.java
+++ src/parkingsimulator/HelloWorld.java
@@ -10,6 +10,7 @@ private String Hello;
 	}
 	public void printHelloDetmer() {
 		System.out.println("Hallo Detmer als je dit ziet dan werkt het.");
+		System.out.println("Hallo als je dit ziet werkt het.");
 	}
 	//Test
 	}
