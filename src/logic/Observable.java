7acf5e0921b79d4c999a3d2bf6167952f0eb80be
Rick Zwaneveld <r.zwaneveld@st.hanze.nl>
2018-01-23 18:42:01 +0100

    More javadoc

--- src/logic/Observable.java
+++ src/logic/Observable.java
@@ -2,9 +2,25 @@ package logic;
 
 import view.Observer;
 
+/**
+ * This is an interface responsible for two things. It makes sure all the observable
+ * are able to register an observer. It also makes sure they can notify the observers.
+ * 
+ * @author Rick Zwaneveld
+ * @version 23-01-2018
+ */
+
 public interface Observable {
 
+	/**
+	 * This method registers a reference between the observable and the observer.
+	 * 
+	 * @param observer the observer
+	 */
 	public void registerObserver (Observer observer);
 	
+	/**
+	 * This method is used to notify the observers whenever this is necessary.
+	 */
 	public void notifyObservers();
 }
--- src/view/Observer.java
+++ src/view/Observer.java
@@ -1,6 +1,18 @@
 package view;
 
+/**
+ * This interface gives the observer an update method. It is used to updated the
+ * observed object whenever their is a change. The new information is then pulled
+ * from the model.
+ * 
+ * @author Rick Zwaneveld
+ * @version 23-01-2018
+ */
+
 public interface Observer {
 	
+	/**
+	 * This method is called upon by an observed object.
+	 */
 	public void update();
 }
