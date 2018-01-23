3c6cceff9ea6bb8b7b432f4189e0d3dae651e710
Rick Zwaneveld <r.zwaneveld@st.hanze.nl>
2018-01-23 18:56:09 +0100

    More javadoc

--- src/model/Location.java
+++ src/model/Location.java
@@ -1,11 +1,28 @@
 package model;
 
+/**
+ * This class keeps track of the locations of parking spots. It has floors, rows and
+ * places. It also provides getter methods to return these numbers. These numbers
+ * can also be returned as a hash code. Lastly this class also provides a quality
+ * check to see if an obj matches Location.
+ * 
+ * @author Rick Zwaneveld
+ * @version 23-01-2018
+ */
+
 public class Location {
 	
     private int floor;
     private int row;
     private int place;
 
+    /**
+     * The constructor of this class.
+     * 
+     * @param floor a number for the floor
+     * @param row a number for the row
+     * @param place a number for the place
+     */
     public Location(int floor, int row, int place) {
         this.floor = floor;
         this.row = row;
@@ -13,9 +30,10 @@ public class Location {
     }
 
     /**
-     * Method that checks if the object created matches the
-     * instance of the Location class. This is used for to
-     * implement content equality.
+     * This method will check if the object created matches the instance of the 
+     * Location class. This is used to implement content equality.
+     * 
+     * @return true if obj matches Location, false if otherwise
      */
     public boolean equals(Object obj) {
         if(obj instanceof Location) {
@@ -27,29 +45,49 @@ public class Location {
         }
     }
     
+    /**
+     * This method will create a string out of the values of floor, row and place
+     * and concatenates them together via a comma.
+     * 
+     * @return a String consisting of the floor, row and place
+     */
     public String toString() {
         return floor + "," + row + "," + place;
     }
 
     /**
-     * Using 10 bits for each of the floor, row and place
-     * values. This should give a unique hash code for each
-     * (floor, row, place) tuple.
+     * This method uses 10 bits for each of the floor, row and place values. This 
+     * should give a unique hash code for each (floor, row, place) tuple.
      * 
-     * @return A hash code for the location.
+     * @return a hash code for the location
      */
     public int hashCode() {
         return (floor << 20) + (row << 10) + place;
     }
 
+    /**
+     * This method will return the number of the floor.
+     * 
+     * @return floor a floor number
+     */
     public int getFloor() {
         return floor;
     }
 
+    /**
+     * This method will return the number of a row.
+     * 
+     * @return row a row number
+     */
     public int getRow() {
         return row;
     }
 
+    /**
+     * This method will return the number of a place.
+     * 
+     * @return place a place number
+     */
     public int getPlace() {
         return place;
     }
