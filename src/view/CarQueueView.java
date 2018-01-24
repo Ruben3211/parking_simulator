ad5a624022dec0b6a06a7b5b969193cfd580d1e2
Joey <joey@astralvision.nl>
2018-01-24 13:08:29 +0100

    Added paymentcarqueue to the view

--- src/view/CarQueueView.java
+++ src/view/CarQueueView.java
@@ -11,7 +11,7 @@ import model.SimulatorModel;
 /*
  * This class makes Jlabel and assigns a text to it that keeps track of the number of cars in the queue.
  * When the update method is called it takes the queue and calls the carInQueue method that returns the size of the array. 
- *  
+ * 
  */
 
 @SuppressWarnings("serial")
@@ -20,6 +20,7 @@ public class CarQueueView extends AbstractView  {
 	public static JLabel regCarQueue;
 	public static JLabel subCarQueue;
 	public static JLabel exitCarQueue;
+	public static JLabel paymentCarQueue;
 
 	public CarQueueView(SimulatorModel simulator) {
 		super(simulator);
@@ -29,19 +30,22 @@ public class CarQueueView extends AbstractView  {
 		regCarQueue = new JLabel("Regular cars in queue: 0");
 		subCarQueue = new JLabel("Cars with subscription in queue: 0");
 		exitCarQueue = new JLabel("Queue for leaving the garage: 0");
-		
+		paymentCarQueue = new JLabel("Queue for paying: 0");
 		
 		
 		this.setLayout(null);
 		add(regCarQueue);
 		add(subCarQueue);
 		add(exitCarQueue);
+		add(paymentCarQueue);
 		regCarQueue.setBounds(400,10, 300, 30);
 		subCarQueue.setBounds(400,20, 300, 30);
 		exitCarQueue.setBounds(400,40, 300, 10);
+		paymentCarQueue.setBounds(400,0, 300, 30);
 		regCarQueue.setVisible(true);
 		subCarQueue.setVisible(true);
 		exitCarQueue.setVisible(true);
+		paymentCarQueue.setVisible(true);
 		
 	}
 	/*
@@ -52,6 +56,7 @@ public class CarQueueView extends AbstractView  {
 		regCarQueue.setText("Regular cars in queue: " + SimulatorModel.entranceRegQueue.carsInQueue());
 		subCarQueue.setText("Cars with subscription in queue: " + SimulatorModel.entranceSubResQueue.carsInQueue());
 		exitCarQueue.setText("Queue for leaving the garage: " + SimulatorModel.exitCarQueue.carsInQueue());
+		paymentCarQueue.setText("Queue for paying: " + SimulatorModel.paymentCarQueue.carsInQueue());
 	}
 
 }
