dba61b871ec18436851151f9a5cc50a06ac21374
Rick Zwaneveld <r.zwaneveld@st.hanze.nl>
2018-01-24 17:41:31 +0100

    Removed space

--- src/main/Simulator.java
+++ src/main/Simulator.java
@@ -51,7 +51,7 @@ public class Simulator {
 		screen.getContentPane().add(carQueueView);
 		
 		screen.getContentPane().add(financialView);
-		financialView.setBounds(1000,60, 600, 100);
+		financialView.setBounds(1000, 60, 600, 100);
 		
 		screen.getContentPane().add(timeView);
 		timeView.setBounds(10, 600, 100, 25);
