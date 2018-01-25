24e27c44c1ca5cb0c8aac169586bebf3c58ad171
Ruben Bonga <r.a.bonga@st.hanze.nl>
2018-01-25 11:19:06 +0100

    eigenlijk niks veranderd

--- src/view/BarChartView.java
+++ src/view/BarChartView.java
@@ -1,12 +1,16 @@
 package view;
 
 import model.SimulatorModel;
+import java.applet.*;
+import java.awt.*;
+
 
 @SuppressWarnings("serial")
 public class BarChartView extends AbstractView {
-	
+
 	public BarChartView(SimulatorModel simulator) {
 		super(simulator);
 		
 	}
 }
+
