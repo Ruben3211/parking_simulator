af47fa7af3824b75946d3dba43693fe6f4a45ac4
Detmer Struiksma <p.detmer56@gmail.com>
2018-01-26 09:57:39 +0100

    JavaDoc toegevoegd aan SliderController class

--- src/controller/SliderController.java
+++ src/controller/SliderController.java
@@ -8,11 +8,23 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 @SuppressWarnings("serial")
+/**
+ * This class is responsible for creating a slider controller that changes the speed of the simulation.
+ * It implements a changelistener that calls a method when the value of the slider gets changed.
+ * 
+ * @author Detmer Struiksma
+ * @version 26-01-2018
+ *
+ */
 
 public class SliderController extends AbstractController implements ChangeListener{
 	
 	private JSlider changeSpeed;
-	
+	/**
+	 * Constructor for the class.
+	 * 
+	 * @param simulator Takes an object of type SimulatorModel to call methods the SimulatorModel class.
+	 */
 	public SliderController(SimulatorModel simulator) {
 		super(simulator);
 		
@@ -20,9 +32,9 @@ public class SliderController extends AbstractController implements ChangeListen
 		changeSpeed = new JSlider(10,200,100);
 		changeSpeed.addChangeListener(this);
 		
-		changeSpeed.setMinorTickSpacing(4);  
-		changeSpeed.setMajorTickSpacing(40);  
-		changeSpeed.setPaintTicks(true);  
+		changeSpeed.setMinorTickSpacing(4); 
+		changeSpeed.setMajorTickSpacing(40);
+		changeSpeed.setPaintTicks(true);
 		changeSpeed.setInverted(true);
 		add(changeSpeed);
 		
@@ -30,7 +42,13 @@ public class SliderController extends AbstractController implements ChangeListen
 		
 		
 	}
-
+    /**
+     * This method is responsible for the speed of the simulation.
+     * It takes the value of the slider and uses a setter for the variable setStepPause to change the speed of the simulation.
+     * 
+     * @param e Is used to notify when the state of the slider has changed.
+     * 
+     */
 	public void stateChanged(ChangeEvent e) {
 		int value = changeSpeed.getValue();
 		simulator.setStepPause(value);
