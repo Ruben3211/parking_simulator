39df581c9b4bc71e1fccafd3236799bb12a24263
Ruben Bonga <r.a.bonga@st.hanze.nl>
2018-01-29 09:24:52 +0100

    3dbar iets aangepast

--- src/view/BarChartView.java
+++ src/view/BarChartView.java
@@ -2,16 +2,29 @@ package view;
 
 import model.SimulatorModel;
 import java.awt.*;
-
-
-
+import javax.swing.JLabel;
+import java.awt.geom.*;
 
 @SuppressWarnings("serial")
 public class BarChartView extends AbstractView{
 
-
-	public BarChartView(SimulatorModel simulator) {
+private JLabel Regcars, Subcars, Rescars, Empty;
+	
+public BarChartView(SimulatorModel simulator) {
 		super(simulator);
+	    Regcars = new JLabel();
+	    Subcars = new JLabel();
+	    Rescars = new JLabel();
+	    Empty = new JLabel();
+	    add(Regcars);
+	    add(Rescars);
+	    add(Subcars);
+	    add(Empty);
+	    Regcars.setBounds(60,270, 80, 100);
+		Subcars.setBounds(400,20, 300, 30);
+		Rescars.setBounds(400,40, 300, 10);
+		Empty.setBounds(400,0, 300, 30);
+		
 	}
 		public void paintComponent(Graphics g) {
 			int reg = getModel().getTotalParkedReg();
@@ -21,18 +34,41 @@ public class BarChartView extends AbstractView{
 			Color a = new Color(238, 238, 238);
 			g.setColor(a);
 			g.fillRect(0, 0, 600, 600);
-			g.setColor(Color.RED);
-			g.fill3DRect(60, 250, 80, reg, false);
+			//g.setColor(Color.RED);
+			//g.fill3DRect(60, 250, 80, reg, false);
 	
-			g.setColor(Color.BLUE);
-		    g.fill3DRect(360, 250, 80, -sub, true);
+			creatBar(g, Color.YELLOW, 160,250 , 80, res);
+			
+			
+			//g.setColor(Color.BLUE);
+		   // g.fill3DRect(360, 250, 80, -sub, true);
 		
-		   g.setColor(Color.YELLOW);
-		   // g.fill3DRect(160, 250, 80, res, true);
-		   g.fillRect(160, 250, 80, -res);
+		  // g.setColor(Color.YELLOW);
+		  //  g.fill3DRect(160, 250, 80, res, true);
+		 //  /g.fillRect(160, 250, 80 - 3, res);
+		
 		    
-		    g.setColor(Color.WHITE);
-		    g.fill3DRect(260, 250, 80, empty, true);
+		    
+		   // g.setColor(Color.WHITE);
+		   // g.fill3DRect(260, 250, 80, empty, true);
+		    
+		    Regcars.setText("Reguluar "+ reg);
+		    Subcars.setText("Subscription "+ sub);
+		    Rescars.setText("Resevation "+ res);
+		    Empty.setText("Empty "+ empty);
+		    
+		    
+		    Regcars.setBounds(60,260, 80, 100);
+			Subcars.setBounds(360,260, 100, 100);
+			Rescars.setBounds(160,260, 80, 100);
+			Empty.setBounds(260,260, 80, 100);
+		 
 		}
-	
+		public void creatBar(Graphics g, Color kleur, int x, int y, int with, int height ) {
+
+		g.setColor(kleur);
+		g.fill3DRect(x, y, with, height, true);
+		
+		
+		}
 }
