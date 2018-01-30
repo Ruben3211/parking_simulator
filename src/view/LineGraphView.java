cdb2d7716619e9d34bec702aaa4ef60c376208b5
Detmer Struiksma <detmer_struiksma@hotmail.com>
2018-01-30 14:43:47 +0100

    Fixed an issue in LineGraphView where it wouldn't draw until the first minute in the simulation.

--- src/view/LineGraphView.java
+++ src/view/LineGraphView.java
@@ -49,6 +49,7 @@ public class LineGraphView extends AbstractView {
 	
     @Override
     protected void paintComponent(Graphics g) {
+    	data.add(0);
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
